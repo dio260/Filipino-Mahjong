9bcfc917de9efbf129369649013f8f9981aa849a
Dion Pimentel <dionpimentel260@gmail.com>
2023-04-09 15:07:20 -0700

    its new build time

--- Assets/Scripts/DrawerOpen.cs
+++ Assets/Scripts/DrawerOpen.cs
@@ -1,16 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DrawerOpen : MonoBehaviour
 {
     public TMPro.TMP_InputField roomInput, nameInput;
     public Animator anim;
+    public Button joinButton;
     public bool open;
     // Start is called before the first frame update
     void Start()
     {
         open = false;
+        joinButton.gameObject.SetActive(false);
     }
 
     // Update is called once per frame
@@ -21,12 +24,16 @@ public class DrawerOpen : MonoBehaviour
             Debug.Log("roomname and player name exists");
             anim.SetTrigger("OpenTrigger");
             open = true;
+            joinButton.gameObject.SetActive(true);
+
         }
         else if ((roomInput.text.Length == 0 || nameInput.text.Length == 0) && open)
         {
             Debug.Log("no room name or player name");
             anim.SetTrigger("OpenTrigger");
             open = false;
+            joinButton.gameObject.SetActive(false);
+
         }
     }
 }
